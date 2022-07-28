7b805ac5c76058e94592eef12c667ec00ed834ca
Because99 <arina@DESKTOP-R6FL5IL.localdomain>
2022-07-28 19:21:41 +0300

    fix package.json

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,4 +1,5 @@
-import React, { useSelector } from 'react-redux';
+import React from 'react'
+import { useSelector } from 'react-redux';
 import { Link, NavLink } from 'react-router-dom';
 
 function Nav() {
