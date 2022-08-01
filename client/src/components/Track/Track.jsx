ede3bb562c40505e17336d3dace19b739c57ece3
IgorGap <ig.gaponenko@gmail.com>
2022-08-01 10:21:33 +0300

    Rel.8

--- client/src/components/Track/Track.jsx
+++ client/src/components/Track/Track.jsx
@@ -41,12 +41,6 @@ function Track() {
       </div>
     </div>
 
-    // <audio controls>
-    //   <track src={audio} type="audio/ogg; codecs=vorbis" />
-    //   <track src={audio} type="audio/mpeg" />
-    //   Тег audio не поддерживается вашим браузером.
-    //   <a href="audio/music.mp3">Скачайте музыку</a>
-    // </audio>
   );
 }
 
