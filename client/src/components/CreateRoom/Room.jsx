f3645cbe4780549eafaca18a5bf3a5180512dbc4
Emiliya <emilya.korkunova@gmail.com>
2022-08-02 10:59:43 +0300

    divide Room into components

--- client/src/components/CreateRoom/Room.jsx
+++ client/src/components/CreateRoom/Room.jsx
@@ -3,17 +3,16 @@ import { useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import socket from '../../socket';
 import Chat from '../Chat/Chat';
-import InputWithButton from '../Forms/InputWithBtn/InputWithButton';
 import Track from '../Track/Track';
 import SearchBar from './SearchBar';
-import Multer from '../Multer/Multer';
 import './Room.css';
 import './CreateRoom.css';
+import GuestsInfo from './GuestsInfo/GuestsInfo';
+import SearchPannel from './SearchPannel/SearchPannel';
 
 function Room() {
   const id = useParams();
   const user = useSelector((state) => (state.user));
-  // const guest = useSelector((state) => state.guest);
   const navigate = useNavigate();
   const [guests, setGuests] = useState([]);
   const [info, setInfo] = useState([]);
@@ -31,97 +30,17 @@ function Room() {
 
   // find All Tracks
 
-  const [audioAll, setAudioAll] = useState();
-
-  const findAudioFetch = async () => {
-    const response = await fetch('http://localhost:3001/audio', {
-      credentials: 'include',
-    });
-    const result = await response.json();
-    console.log(result);
-    setAudioAll(result);
-  };
-
-  useEffect(() => {
-    findAudioFetch();
-  }, []);
-
-  console.log('+++++++++++++', info);
-  useEffect(() => {
-    console.log('front', guests);
-  });
-
-  useEffect(() => {
-    socket.on('recieve_guest', (guest) => {
-      console.log(guest);
-      setGuests((prev) => [...prev, guest]);
-    });
-  }, [socket]);
-
-  const deleteRoomHandler = async () => {
-    const response = await fetch(`http://localhost:3001/room/${id.id}`, {
-      credentials: 'include',
-      method: 'DELETE',
-    });
-    navigate('/');
-  };
-
-  const exitRoomHandler = async () => {
-    const response = await fetch(`http://localhost:3001/room/${id.id}`, {
-      credentials: 'include',
-      method: 'DELETE',
-    });
-    navigate('/');
-  };
-
   if (info.info) {
     return (
       <div className="private">
-        <div className="table1">
-          <div className="card room-creator-card bg-base-100 shadow-xl">
-            <div className="card-body">
-              <h2 className="card-title">
-                Room name:
-                {' '}
-                {info.info.roomName}
-              </h2>
-              <h2 className="card-title">
-                Creator:
-                {' '}
-                {info.authorRoom.userName}
-              </h2>
-              <br />
-              {info.authorRoom.id === user.id
-                && (
-                  <div>
-                    <button type="submit" className="btn btn-ghost ">Start</button>
-                    <button type="submit" className="btn btn-ghost ">Pause</button>
-                    <button type="submit" className="btn btn-ghost ">Stop</button>
-                  </div>
-                )}
+        <GuestsInfo
+          info={info}
 
-            </div>
-          </div>
-          <div className="card guests-card bg-base-100 shadow-xl">
-            <div className="card-body scroll-block">
-              <h2 className="card-title">Гости</h2>
-              <div className="btn-group" />
-              {guests.map((el) => (<p>{el}</p>))}
-              <div className="card-actions justify-end" />
-            </div>
-          </div>
-          <div className="btn1">
-            {info.authorRoom.id === user.id
-              && <button type="submit" onClick={deleteRoomHandler} className="btn btn-primary ">Delete room</button>}
-            {info.authorRoom.id !== user.id && <button type="submit" onClick={exitRoomHandler} className="btn">leave room</button>}
-          </div>
-        </div>
+        />
         <div className="track">
-          <SearchBar placeholder="Enter a auidio name..." audioAll={audioAll} />
-          <ul className="tracklist scroll-block">
-            <li className="track"><Track /></li>
-          </ul>
+          <SearchPannel />
         </div>
+        {/* Компонент с плеером */}
         <div className="chat">
           <div className="mockup-phone">
             <div className="camera" />
