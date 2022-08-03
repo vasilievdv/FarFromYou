7233caa567ce6cbe2bcb5913320a8ee8f829f9fb
Because99 <arina@DESKTOP-R6FL5IL.localdomain>
2022-08-03 16:03:34 +0300

    f

--- server/src/routes/room.router.js
+++ server/src/routes/room.router.js
@@ -23,26 +23,14 @@ router.get('/:id', checkGuestOrAuthor, async (req, res) => {
 
   const nameCreater = creater[0]['User.userName'];
   const nemeRoom = creater[0]['Room.roomName'];
-  // console.log('+++++++', guests[0]);
+
   let arrGuest = [];
   if (guests[0]) {
-    // console.log('555555');
     arrGuest = guests.map((el) => el['User.userName']);
-    // console.log({ nameCreater, nemeRoom, arrGuest });
-    // return res.json({ nameCreater, nemeRoom, arrGuest });
   }
-  // console.log('444444');
-  // console.log({ nameCreater, nemeRoom, arrGuest });
+
   return res.json({ nameCreater, nemeRoom, arrGuest });
 });
-// console.log(nameCreater, nemeRoom, arrGuest);
-// const idGuoists = guests.map((el) => el.id);
-// const info = await Room.findOne({ where: { id } });
-// const authorRoom = await User.findOne({ where: { id: info.user_id } });
-// console.log('_________', guests, creater);
-//   const res1 = idUser.filter((el) => el.id === idGuoists.el);
-//   const info = await User.findAll({ where: { id: idGuoists.map((el) => el) } });
-// el['User.id, User.name']
 
 router.delete('/:id', checkAuth, async (req, res) => {
   const { id } = req.params;
